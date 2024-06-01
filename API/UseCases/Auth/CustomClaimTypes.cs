2f4ba70173985b7e26845cb33a917697b4ab6a3c
brunonoatto <brunonoatto@gmail.com>
2024-06-01 01:16:13 -0300

    fix: change TokenData to class and implement GetClaims method

---
+++ API/UseCases/Auth/CustomClaimTypes.cs
@@ -0,0 +1,9 @@
+namespace AutologApi.API.UseCases.Auth
+{
+    public static class CustomClaimTypes
+    {
+        public const string Id = "id";
+        public const string Name = "name";
+        public const string Type = "type";
+    }
+}
--- API/UseCases/Auth/TokenData.cs
+++ API/UseCases/Auth/TokenData.cs
@@ -1,6 +1,21 @@
+using System.Security.Claims;
 using AutologApi.API.Domain.Models;
+using AutologApi.API.UseCases.Auth;
 
 namespace AutologApi.API.UseCases
 {
-    public record TokenData(string Id, string Name, UserTypeEnum Type);
+    public class TokenData(string Id, string Name, UserTypeEnum Type)
+    {
+        public IEnumerable<Claim> GetClaims()
+        {
+            var result = new List<Claim>
+            {
+                new(CustomClaimTypes.Id, Id),
+                new(CustomClaimTypes.Name, Name),
+                new(CustomClaimTypes.Type, Type.ToString()),
+            };
+
+            return result;
+        }
+    }
 }
