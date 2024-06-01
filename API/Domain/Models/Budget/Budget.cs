26df29621414771c78505c18d3cbd0b6bbcd09fa
brunonoatto <brunonoatto@gmail.com>
2024-06-01 01:04:16 -0300

    feat: remake Budget entity and create metod to get link to WhatsApp

--- API/Domain/Models/Budget/Budget.cs
+++ API/Domain/Models/Budget/Budget.cs
@@ -1,10 +1,11 @@
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+using System.Web;
 
 namespace AutologApi.API.Domain.Models
 {
     public enum BudgetStatusEnum
     {
+        None = 0,
         MakingBudget = 1,
         WaitingBudgetApproval = 2,
         ApprovedBudget = 3,
@@ -14,19 +15,35 @@ namespace AutologApi.API.Domain.Models
         Finished = 7,
     }
 
-    [PrimaryKey(nameof(Id), nameof(Os))]
     public class Budget : EntityBase
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Os { get; set; }
+        public int Os { get; set; }
         public required Guid GarageId { get; set; }
-        public required Guid ClientId { get; set; }
+        public required Guid UserId { get; set; }
         public required Guid CarId { get; set; }
         public required BudgetStatusEnum Status { get; set; }
-        public required string Observation { get; set; }
+        public string? Observation { get; set; }
 
-        public User? Client { get; set; }
-        public Garage? Garage { get; set; }
-        public Car? Car { get; set; }
+        public virtual User? User { get; set; }
+        public virtual Garage? Garage { get; set; }
+        public virtual Car? Car { get; set; }
+        public virtual ICollection<BudgetItem> Items { get; set; } = [];
+
+        public string GetWhatsAppBudgetLink()
+        {
+            var link =
+                User?.Email != null
+                    ? $"http://127.0.0.1:5173/cliente/orcamento/{Os}"
+                    : $"http://127.0.0.1:5173/orcamento/{Os}";
+
+            var msg =
+                $"Olá {User?.Name}, aqui é da mecênica {Garage?.User?.Name}.\n\nSeu orçamento está pronto, basta clicar no link abaixo para revisar e aprovar.\nIniciaremos o serviço mediante aprovação do orçamento.\n\nLink: {link}";
+
+            var whatsAppLink =
+                $"https://web.whatsapp.com/send/?phone=55{User?.Phone}&text={HttpUtility.UrlEncode(msg)}&type=phone_number&app_absent=0";
+
+            return whatsAppLink;
+        }
     }
 }
