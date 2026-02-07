a29460008c5372fa22a384d14c55af124e356d32
brunonoatto <brunonoatto@gmail.com>
2026-02-07 16:57:29 -0300

    fix: permit send decimal in price to add busdet item

--- API/UseCases/BudgetItem/CreateBudgetItem/CreateBudgetItemUseCaseInput.cs
+++ API/UseCases/BudgetItem/CreateBudgetItem/CreateBudgetItemUseCaseInput.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AutologApi.API.UseCases
 {
-    public record BudgetItemUseCaseInput(string Id, string Description, int Qtd, int Price);
+    public record BudgetItemUseCaseInput(string Id, string Description, int Qtd, decimal Price);
 
     public record CreateBudgetItemUseCaseInput(
         [FromBody] BudgetItemUseCaseInput BodyData,
