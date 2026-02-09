8a4d6b2873062d7b30f1401d563c0da65ea2a7f6
brunonoatto <brunonoatto@gmail.com>
2026-02-08 23:46:47 -0300

    migration: RemoveBrandColumnOfCar

---
+++ Migrations/20260209022931_RemoveBrandColumnOfCar.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutologApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBrandColumnOfCar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Brand", table: "Cars");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Cars",
+                type: "text",
+                nullable: false,
+                defaultValue: ""
+            );
+        }
+    }
+}
